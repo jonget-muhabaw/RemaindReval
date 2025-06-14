541a599011bfba25782eb3eef2e8980a58817d21
unknown <user@IT-ESUBALEW.RAVALSTEEL.LOCAL>
2025-06-14 12:24:09 +0300

    feat:login page

---
+++ src/presentation/components/Login.tsx
@@ -0,0 +1,39 @@
+
+
+const Login = () => {
+  return (
+ <> 
+<div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className=" max-w-sm bg-white border border-gray-200 rounded-lg  shadow">
+    <form action="#" className="space-y-6">
+     <h1 className="font-medium text-gray-900 text-xl">Login to RavalRemainder</h1>
+     <div>
+        <label   className="block text-gray-900 text-sm mb-2 text-medium"></label>
+        <input type="email" id="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+     </div>
+      <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+        </div>
+        <div className="flex items-start">
+            <div className="flex items-start">
+                <div className="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                </div>
+                <label  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+            </div>
+            <a href="#" className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+        </div>
+        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+        </div>
+
+    </form>
+</div>
+</div>
+ </>
+  )
+}
+
+export default Login
