d8424ee0c898d203ca0325473c71282003b6f7b9
Alemayehu <alluwa2127@gmail.com>
2025-06-17 16:41:47 +0300

    feat: add logo icon

--- src/presentation/components/Login.tsx
+++ src/presentation/components/Login.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 sm:p-6">
@@ -45,12 +47,16 @@ const Login = () => {
               Forgot Password?
             </a>
           </div>
-          <button
-            type="submit"
-            className="w-full text-white bg-primary hover:secondary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+          <Link
+            to="/dashboard"
+            className="block w-full text-white bg-primary hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            onClick={() => {
+              console.log("Navigating to dashboard");
+            }}
           >
-            Login to your account
-          </button>
+            Login
+          </Link>
+
           <div className="flex justify-center mt-6">
             <button className="flex items-center justify-center w-full bg-white border border-gray-300 text-primary font-medium rounded-lg text-sm px-5 py-2.5 shadow hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300">
               <img
--- src/presentation/components/Settings.tsx
+++ src/presentation/components/Settings.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Settings = () => {
   return (
     <div>
-      
+      <h1 className='text-2xl text-center font-bold text-primary mt-4'>You adjust your settings for notification in here</h1>
     </div>
   )
 }
